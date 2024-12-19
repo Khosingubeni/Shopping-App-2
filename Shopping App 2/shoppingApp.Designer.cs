743ab5a7d0161d78746617581d51b2c14902a603
khosi <khosi@KHOSI>
2024-12-19 14:07:55 +0200

    Add project files.

---
+++ Shopping App 2/shoppingApp.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Shopping_App_2
+{
+    partial class shoppingApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.turntablePbx = new System.Windows.Forms.PictureBox();
+            this.solangePbx = new System.Windows.Forms.PictureBox();
+            this.shafiqPbx = new System.Windows.Forms.PictureBox();
+            this.kaidiPbx = new System.Windows.Forms.PictureBox();
+            this.kendrickPbx = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.slipRichTbx = new System.Windows.Forms.RichTextBox();
+            this.dueLb = new System.Windows.Forms.Label();
+            this.buyBtn = new System.Windows.Forms.Button();
+            this.cashlb = new System.Windows.Forms.Label();
+            this.cashTxb = new System.Windows.Forms.TextBox();
+            this.changelb = new System.Windows.Forms.Label();
+            this.nextBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.turntablePbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solangePbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shafiqPbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kaidiPbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kendrickPbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Gill Sans MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(53, 476);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "The Loop";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gill Sans MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(256, 476);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "When I Get Home";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gill Sans MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(255, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "In Search Of Hope";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gill Sans MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 312);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "To Pimp A Butterfly";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(152, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Record Mad!";
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::Shopping_App_2.Properties.Resources.headphones_icon_logo_vector_design_165560217;
+            this.pictureBox7.Location = new System.Drawing.Point(459, 21);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(166, 117);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 15;
+            this.pictureBox7.TabStop = false;
+            // 
+            // turntablePbx
+            // 
+            this.turntablePbx.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.turntablePbx.Image = global::Shopping_App_2.Properties.Resources.P1;
+            this.turntablePbx.Location = new System.Drawing.Point(44, 507);
+            this.turntablePbx.Name = "turntablePbx";
+            this.turntablePbx.Size = new System.Drawing.Size(321, 165);
+            this.turntablePbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.turntablePbx.TabIndex = 10;
+            this.turntablePbx.TabStop = false;
+            this.turntablePbx.Click += new System.EventHandler(this.turntablePbx_Click);
+            // 
+            // solangePbx
+            // 
+            this.solangePbx.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.solangePbx.Image = global::Shopping_App_2.Properties.Resources.SOLANGE;
+            this.solangePbx.Location = new System.Drawing.Point(226, 341);
+            this.solangePbx.Name = "solangePbx";
+            this.solangePbx.Size = new System.Drawing.Size(163, 132);
+            this.solangePbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.solangePbx.TabIndex = 11;
+            this.solangePbx.TabStop = false;
+            this.solangePbx.Click += new System.EventHandler(this.solangePbx_Click);
+            // 
+            // shafiqPbx
+            // 
+            this.shafiqPbx.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.shafiqPbx.Image = global::Shopping_App_2.Properties.Resources.SHAFIQ;
+            this.shafiqPbx.Location = new System.Drawing.Point(16, 341);
+            this.shafiqPbx.Name = "shafiqPbx";
+            this.shafiqPbx.Size = new System.Drawing.Size(163, 132);
+            this.shafiqPbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.shafiqPbx.TabIndex = 12;
+            this.shafiqPbx.TabStop = false;
+            this.shafiqPbx.Click += new System.EventHandler(this.shafiqPbx_Click);
+            // 
+            // kaidiPbx
+            // 
+            this.kaidiPbx.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kaidiPbx.Image = global::Shopping_App_2.Properties.Resources.in_search_of_hope;
+            this.kaidiPbx.Location = new System.Drawing.Point(226, 167);
+            this.kaidiPbx.Name = "kaidiPbx";
+            this.kaidiPbx.Size = new System.Drawing.Size(163, 142);
+            this.kaidiPbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kaidiPbx.TabIndex = 13;
+            this.kaidiPbx.TabStop = false;
+            this.kaidiPbx.Click += new System.EventHandler(this.kaidiPbx_Click);
+            // 
+            // kendrickPbx
+            // 
+            this.kendrickPbx.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kendrickPbx.Image = global::Shopping_App_2.Properties.Resources.kendrick_2;
+            this.kendrickPbx.Location = new System.Drawing.Point(16, 170);
+            this.kendrickPbx.Name = "kendrickPbx";
+            this.kendrickPbx.Size = new System.Drawing.Size(163, 142);
+            this.kendrickPbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kendrickPbx.TabIndex = 14;
+            this.kendrickPbx.TabStop = false;
+            this.kendrickPbx.Click += new System.EventHandler(this.kendrickPbx_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Shopping_App_2.Properties.Resources.VinylHub_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(318, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Gill Sans MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(153, 675);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Turn Table P1";
+            // 
+            // slipRichTbx
+            // 
+            this.slipRichTbx.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.slipRichTbx.Font = new System.Drawing.Font("Gill Sans MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.slipRichTbx.Location = new System.Drawing.Point(459, 170);
+            this.slipRichTbx.Name = "slipRichTbx";
+            this.slipRichTbx.ReadOnly = true;
+            this.slipRichTbx.Size = new System.Drawing.Size(190, 293);
+            this.slipRichTbx.TabIndex = 16;
+            this.slipRichTbx.Text = "";
+            // 
+            // dueLb
+            // 
+            this.dueLb.AutoSize = true;
+            this.dueLb.Font = new System.Drawing.Font("Gill Sans MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dueLb.Location = new System.Drawing.Point(460, 487);
+            this.dueLb.Name = "dueLb";
+            this.dueLb.Size = new System.Drawing.Size(39, 21);
+            this.dueLb.TabIndex = 7;
+            this.dueLb.Text = "Due";
+            // 
+            // buyBtn
+            // 
+            this.buyBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buyBtn.Location = new System.Drawing.Point(464, 555);
+            this.buyBtn.Name = "buyBtn";
+            this.buyBtn.Size = new System.Drawing.Size(161, 26);
+            this.buyBtn.TabIndex = 18;
+            this.buyBtn.Text = "Buy";
+            this.buyBtn.UseVisualStyleBackColor = true;
+            this.buyBtn.Click += new System.EventHandler(this.buyBtn_Click);
+            // 
+            // cashlb
+            // 
+            this.cashlb.AutoSize = true;
+            this.cashlb.Font = new System.Drawing.Font("Gill Sans MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cashlb.Location = new System.Drawing.Point(460, 518);
+            this.cashlb.Name = "cashlb";
+            this.cashlb.Size = new System.Drawing.Size(44, 21);
+            this.cashlb.TabIndex = 7;
+            this.cashlb.Text = "Cash";
+            // 
+            // cashTxb
+            // 
+            this.cashTxb.Location = new System.Drawing.Point(561, 518);
+            this.cashTxb.Name = "cashTxb";
+            this.cashTxb.Size = new System.Drawing.Size(64, 20);
+            this.cashTxb.TabIndex = 17;
+            // 
+            // changelb
+            // 
+            this.changelb.AutoSize = true;
+            this.changelb.Font = new System.Drawing.Font("Gill Sans MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changelb.Location = new System.Drawing.Point(517, 593);
+            this.changelb.Name = "changelb";
+            this.changelb.Size = new System.Drawing.Size(63, 21);
+            this.changelb.TabIndex = 7;
+            this.changelb.Text = "Change";
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextBtn.Location = new System.Drawing.Point(482, 630);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(143, 28);
+            this.nextBtn.TabIndex = 18;
+            this.nextBtn.Text = "Next Customer";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // shoppingApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(674, 696);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.buyBtn);
+            this.Controls.Add(this.cashTxb);
+            this.Controls.Add(this.slipRichTbx);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.turntablePbx);
+            this.Controls.Add(this.solangePbx);
+            this.Controls.Add(this.shafiqPbx);
+            this.Controls.Add(this.kaidiPbx);
+            this.Controls.Add(this.kendrickPbx);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.changelb);
+            this.Controls.Add(this.cashlb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dueLb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "shoppingApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vinyl Hub";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.turntablePbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solangePbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shafiqPbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kaidiPbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kendrickPbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox turntablePbx;
+        private System.Windows.Forms.PictureBox solangePbx;
+        private System.Windows.Forms.PictureBox shafiqPbx;
+        private System.Windows.Forms.PictureBox kaidiPbx;
+        private System.Windows.Forms.PictureBox kendrickPbx;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox slipRichTbx;
+        private System.Windows.Forms.Label dueLb;
+        private System.Windows.Forms.Button buyBtn;
+        private System.Windows.Forms.Label cashlb;
+        private System.Windows.Forms.TextBox cashTxb;
+        private System.Windows.Forms.Label changelb;
+        private System.Windows.Forms.Button nextBtn;
+    }
+}
+
---
+++ Shopping App 2/shoppingApp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shopping_App_2
+{
+    public partial class shoppingApp : Form
+    {
+        public static double due = 0, change;
+        public shoppingApp()
+        {
+            InitializeComponent();
+        }
+
+        private void kendrickPbx_Click(object sender, EventArgs e)
+        {
+            slipRichTbx.Text = slipRichTbx.Text + "\nKendrick - To Pimp A Butterfly: R1050.00";
+            due = due + 1050.00;
+            dueLb.Text = "R" + due.ToString();
+        }
+
+        private void kaidiPbx_Click(object sender, EventArgs e)
+        {
+            slipRichTbx.Text = slipRichTbx.Text + "\nKaidi Tatham - In Search Of Hope:  R1200.00";
+            due = due + 1200.00;
+            dueLb.Text = "R" + due.ToString();
+        }
+
+        private void shafiqPbx_Click(object sender, EventArgs e)
+        {
+            slipRichTbx.Text = slipRichTbx.Text + "\nShafiq Husayn - The Loop: R1400.00";
+            due = due + 1400.00;
+            dueLb.Text = "R" + due.ToString();
+        }
+
+        private void solangePbx_Click(object sender, EventArgs e)
+        {
+            slipRichTbx.Text = slipRichTbx.Text + "\nSolange - When I get Home: R1000.00";
+            due = due + 1000.00;
+            dueLb.Text = "R" + due.ToString();
+        }
+
+        private void buyBtn_Click(object sender, EventArgs e)
+        {
+            double cash = Convert.ToDouble(cashTxb.Text);
+            change = cash - due;
+            changelb.Text = "R" + change.ToString();
+
+            if(change < 0)
+            {
+                MessageBox.Show("Not Enough Cash");
+            }
+            if (cashTxb.Text.Length < 0)
+            {
+                MessageBox.Show("Amount Required!");
+            }
+            
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            change = 0;
+            due= 0;
+            slipRichTbx.Text = "";
+            dueLb.Text = "";
+            cashTxb.Text = "";
+            changelb.Text = change.ToString();
+
+
+        }
+
+        private void turntablePbx_Click(object sender, EventArgs e)
+        {
+            slipRichTbx.Text = slipRichTbx.Text + "\nTurn-table:   R5000.00";
+            due = due + 5000.00;
+            dueLb.Text = "R" + due.ToString();
+        }
+    }
+}
